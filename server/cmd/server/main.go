7ec75a78f2363a11b087eb58af1dddfe65ca2e73
Yernur <yerixstyles@gmail.com>
2026-05-26 17:34:25 +0500

    feat(server): implement StreamTelemtry gRPC receiver loop to process real-time engine frames

--- server/cmd/server/main.go
+++ server/cmd/server/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net"
 
@@ -14,6 +15,46 @@ type simulationServer struct {
 	pb.UnimplementedSimulationServiceServer
 }
 
+// StreamTelemetry implements the client-to-server streaming RPC method
+func (s *simulationServer) StreamTelemetry(stream pb.SimulationService_StreamTelemetryServer) error {
+	fmt.Println("\n Incoming telemetry stream initialized from C++ Engine...")
+
+	var frameCount int
+
+	for {
+		// reading the frame from stream
+		frame, err := stream.Recv()
+
+		if err == io.EOF {
+			fmt.Printf("\n🏁 C++ Engine finished streaming. Total frames processed: %d\n", frameCount)
+
+			response := &pb.SimulationResponse{
+				Success:    true,
+				SimMessage: "Data bridge stable. All frames safely captured by Go Hub!",
+			}
+
+			// sending acknowledgement back to C++ and close the RPC call session
+			return stream.SendAndClose(response)
+		}
+
+		if err != nil {
+			log.Printf("❌ Critical error reading from telemetry stream: %v", err)
+			return err
+		}
+
+		frameCount++
+
+		// Unpacking and printing the frame meta-information
+		fmt.Printf(" [Frame %d] Timestamp: %.3fs | Contains %d Celestial Bodies\n", frame.FrameNumber, frame.Timestamp, len(frame.Bodies))
+
+		// Loop through the unpacked bodies to view the physical states in real time
+		for _, body := range frame.Bodies {
+			fmt.Printf(" Body ID %d: Mass=%1.e | Pos=(%6.1f, %6.1f) | Vel=(%5.1f, %5.1f)\n",
+				body.Id, body.Mass, body.X, body.Y, body.Vx, body.Vy)
+		}
+	}
+}
+
 func main() {
 	fmt.Println("Initializing Go Command Server...")
 
@@ -23,7 +64,7 @@ func main() {
 	// TCP work listener
 	listener, err := net.Listen("tcp", port)
 	if err != nil {
-		log.Fatalf("Failed to bind to port %s: %v", port, err)
+		log.Fatalf("❌ Failed to bind to port %s: %v", port, err)
 	}
 
 	grpcServer := grpc.NewServer()
@@ -35,6 +76,6 @@ func main() {
 	fmt.Printf("gRPC Engine Hub listening on port %s...\n", port)
 
 	if err := grpcServer.Serve(listener); err != nil {
-		log.Fatalf("Failed to activate gRPC engine processing loop: %v", err)
+		log.Fatalf("❌ Failed to activate gRPC engine processing loop: %v", err)
 	}
 }
